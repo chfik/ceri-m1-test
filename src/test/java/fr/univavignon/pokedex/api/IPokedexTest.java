a827a82b193ddadc667ad11bf71cff75208a0ce7
PTC <PTC@10.122.4.64>
2017-03-29 17:40:53 +0200

    error fix

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -1,9 +1,5 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.runner.*;
-import org.mockito.runners.*;
-
-@RunWith(MockitoJUnitRunner.class)
 public class IPokedexTest {
 	
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -3,14 +3,17 @@ package fr.univavignon.pokedex.api;
 import org.junit.*;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 
 public class IPokemonMetadataProviderTest {
 	@Mock private IPokemonMetadataProvider pokemonMetadataProviderMock;
+	@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 	
 	@Before
 	public void setUp() throws PokedexException {
-			Mockito.when(pokemonMetadataProviderMock.getPokemonMetadata(-1)).thenThrow(new PokedexException(""));
+		Mockito.when(pokemonMetadataProviderMock.getPokemonMetadata(-1)).thenThrow(new PokedexException(""));
 	}
 	
 	@Test(expected=PokedexException.class)
