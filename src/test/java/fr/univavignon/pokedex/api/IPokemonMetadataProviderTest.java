c5c5eda0a0d5cba2145208e1484cec31bffbc5b8
PTC <PTC@10.122.4.64>
2017-04-05 14:16:05 +0200

    Add tests pokemon metadata

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,5 +1,7 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.Assert.*;
+
 import org.junit.*;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -14,11 +16,27 @@ public class IPokemonMetadataProviderTest {
 	@Before
 	public void setUp() throws PokedexException {
 		Mockito.when(pokemonMetadataProviderMock.getPokemonMetadata(-1)).thenThrow(new PokedexException(""));
+		Mockito.when(pokemonMetadataProviderMock.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0,"Bulbizarre",126,126,90));
+		Mockito.when(pokemonMetadataProviderMock.getPokemonMetadata(151)).thenThrow(new PokedexException(""));
 	}
 	
 	@Test(expected=PokedexException.class)
-	public void testPokemonNotFoundException() throws PokedexException {
+	public void testPokemonMetadata0NotFoundException() throws PokedexException {
 		pokemonMetadataProviderMock.getPokemonMetadata(-1);
 	}
+
+	@Test(expected=PokedexException.class)
+	public void testPokemonMetadata151NotFoundException() throws PokedexException {
+		pokemonMetadataProviderMock.getPokemonMetadata(151);
+	}
 	
+	@Test 
+	public void testPokemonMetadata() throws PokedexException{
+		PokemonMetadata pokemonMetadata = pokemonMetadataProviderMock.getPokemonMetadata(0);
+		assertEquals(126,pokemonMetadata.getAttack());
+		assertEquals(126,pokemonMetadata.getDefense());
+		assertEquals(0,pokemonMetadata.getIndex());
+		assertEquals("Bulbizarre",pokemonMetadata.getName());
+		assertEquals(90,pokemonMetadata.getStamina());
+	}
 }
