4cac0860d94b92c93648338ac82423cf85cc7e85
PTC <PTC@DESKTOP-KQVOUSK>
2017-05-03 21:51:37 +0200

    Test fix Pokemon Trainer FACTORY

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -2,6 +2,9 @@ package fr.univavignon.pokedex.api;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,8 +22,8 @@ public class IPokemonTrainerFactoryTest {
 	
 	@Before
 	public void setUp() throws PokedexException {
-		PokemonTrainer pokemonTrainer = new PokemonTrainer("trainer", Team.INSTINCT, pokedexMock);
-		Mockito.when(pokemonTrainerFactoryMock.createTrainer("trainer", Team.INSTINCT, pokedexFactoryMock)).thenReturn(pokemonTrainer);
+		PokemonTrainer pokemonTrainer = new PokemonTrainer("trainer2", Team.INSTINCT, pokedexMock);
+		Mockito.when(pokemonTrainerFactoryMock.createTrainer("trainer2", Team.INSTINCT, pokedexFactoryMock)).thenReturn(pokemonTrainer);
 		Mockito.when(pokedexMock.size()).thenReturn(1);
 		Mockito.when(pokedexMock.addPokemon(pokemon)).thenReturn(0);
 		Mockito.when(pokedexMock.getPokemon(0)).thenReturn(pokemon);
@@ -31,18 +34,26 @@ public class IPokemonTrainerFactoryTest {
 	@Test 
 	public void testPokemonTrainer() throws PokedexException {
 		
-		PokemonTrainer pokemonTrainer = pokemonTrainerFactoryMock.createTrainer("trainer", Team.INSTINCT, pokedexFactoryMock);
-		assertEquals("trainer", pokemonTrainer.getName());
+		PokemonTrainer pokemonTrainer = pokemonTrainerFactoryMock.createTrainer("trainer2", Team.INSTINCT, pokedexFactoryMock);
+		assertEquals("trainer2", pokemonTrainer.getName());
 		assertEquals(Team.INSTINCT, pokemonTrainer.getTeam());
 		int index = pokemonTrainer.getPokedex().addPokemon(pokemon);
 		assertEquals(1, pokemonTrainer.getPokedex().size());
 		
-		PokemonTrainer pokemonTrainer2 = pokemonTrainerFactoryMock.createTrainer("trainer", Team.INSTINCT, pokedexFactoryMock);
-		assertEquals("trainer", pokemonTrainer2.getName());
+		PokemonTrainer pokemonTrainer2 = pokemonTrainerFactoryMock.createTrainer("trainer2", Team.INSTINCT, pokedexFactoryMock);
+		assertEquals("trainer2", pokemonTrainer2.getName());
 		assertEquals(Team.INSTINCT, pokemonTrainer2.getTeam());
 		assertEquals(1, pokemonTrainer2.getPokedex().size());
 		assertEquals(pokemon.getName(), pokemonTrainer2.getPokedex().getPokemon(index).getName());
 		assertEquals(pokemon.getIndex(), pokemonTrainer2.getPokedex().getPokemon(index).getIndex());
 		
 	}
+	
+	@After
+	public void eraseFiles(){
+	
+		File trainer2 = new File("trainers/trainer2.ser");
+		trainer2.delete();
+	}
+	
 }
