51c442a195632a1d83e772c432cbbdbfac2fd446
PTC <PTC@DESKTOP-KQVOUSK>
2017-05-04 09:30:09 +0200

    Test les comparator dans IPokedex

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -16,18 +16,34 @@ import org.mockito.junit.MockitoRule;
 public class IPokedexTest {
 	@Mock protected IPokedex pokedexMock;
 	@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+	private Pokemon bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+	private Pokemon aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
 	
-	private Pokemon pokemon = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
 	private List<Pokemon> pokemons = new ArrayList<Pokemon>();
 	
 	@Before
 	public void setUp() throws PokedexException {
-		pokemons.add(pokemon);
+		pokemons.add(bulbizarre);
 		Mockito.when(pokedexMock.size()).thenReturn(0);
-		Mockito.when(pokedexMock.addPokemon(pokemon)).thenReturn(0);
-		Mockito.when(pokedexMock.getPokemon(0)).thenReturn(pokemon);
+		Mockito.when(pokedexMock.addPokemon(bulbizarre)).thenReturn(0);
+		Mockito.when(pokedexMock.addPokemon(aquali)).thenReturn(1);
+		Mockito.when(pokedexMock.getPokemon(0)).thenReturn(bulbizarre);
 		Mockito.when(pokedexMock.getPokemons()).thenReturn(pokemons);
 		Mockito.when(pokedexMock.getPokemon(-1)).thenThrow(new PokedexException(""));
+		
+		List<Pokemon> pokemonsSortedByName = new ArrayList<Pokemon>();
+		pokemonsSortedByName.add(aquali);
+		pokemonsSortedByName.add(bulbizarre);
+		List<Pokemon> pokemonsSortedByCP = new ArrayList<Pokemon>();
+		pokemonsSortedByCP.add(bulbizarre);
+		pokemonsSortedByCP.add(aquali);
+		List<Pokemon> pokemonsSortedByIndex = new ArrayList<Pokemon>();
+		pokemonsSortedByIndex.add(bulbizarre);
+		pokemonsSortedByIndex.add(aquali);
+		Mockito.when(pokedexMock.getPokemons(PokemonComparators.NAME)).thenReturn(pokemonsSortedByName);
+		Mockito.when(pokedexMock.getPokemons(PokemonComparators.CP)).thenReturn(pokemonsSortedByCP);
+		Mockito.when(pokedexMock.getPokemons(PokemonComparators.INDEX)).thenReturn(pokemonsSortedByIndex);
 	}
 	
 	@Test(expected=PokedexException.class)
@@ -39,9 +55,36 @@ public class IPokedexTest {
 	public void testPokedex() throws PokedexException{
 		
 		assertEquals(0, pokedexMock.size());
-		assertEquals(0, pokedexMock.addPokemon(pokemon));
-		assertEquals(pokemon, pokedexMock.getPokemon(0));
+		assertEquals(0, pokedexMock.addPokemon(bulbizarre));
+		assertEquals(bulbizarre, pokedexMock.getPokemon(0));
 		assertEquals(1, pokedexMock.getPokemons().size());
+		
+	}
+	
+	@Test 
+	public void testCPComparatorPokedex() {
+		
+		assertEquals(0, pokedexMock.size());
+		assertEquals(0, pokedexMock.addPokemon(bulbizarre));
+		assertEquals(1, pokedexMock.addPokemon(aquali));
+		
+		List<Pokemon> pokemonsSortedByCP = pokedexMock.getPokemons(PokemonComparators.CP);
+		assertEquals(0, pokemonsSortedByCP.get(0).getIndex());
+		assertEquals(133, pokemonsSortedByCP.get(1).getIndex());
+		
+	}
+	
+	
+	@Test 
+	public void testIndexComparatorPokedex() {
+		
+		assertEquals(0, pokedexMock.size());
+		assertEquals(0, pokedexMock.addPokemon(bulbizarre));
+		assertEquals(1, pokedexMock.addPokemon(aquali));
+		
+		List<Pokemon> pokemonsSortedByIndex = pokedexMock.getPokemons(PokemonComparators.INDEX);
+		assertEquals(0, pokemonsSortedByIndex.get(0).getIndex());
+		assertEquals(133, pokemonsSortedByIndex.get(1).getIndex());
 		
 	}
 }
